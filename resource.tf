9e8a93560b9b456e7a329c9d0b792fd89d3a023c
hemanthkatta07@gmail.com <hemanthkatta07@gmail.com>
2025-07-17 04:51:52 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -2,7 +2,3 @@ resource "azurerm_resource_group" "terraazure" {
   name     = "terraazure"
   location = "East US"
 }
-resource "azurerm_resource_group" "terranow" {
-  name     = "terranow"
-  location = "East US"
-}
